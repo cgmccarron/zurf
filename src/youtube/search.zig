99c0469f440147c501fe3c0682dade2c5b5b7b64
cgmccarron <mccarron.christian13@gmail.com>
2026-05-04 20:27:46 -0600

    added dotenv and ignored it

--- src/youtube/search.zig
+++ src/youtube/search.zig
@@ -4,12 +4,13 @@ const YtTypes = @import("types.zig");
 
 // Search the Youtube API and return a list of videos to play
 fn search() YtTypes.YtVideo {
+    // fake thumbnail for now
     const default_thumbnail = YtTypes.YtThumbnail{
         .url = "https://www.youtubethumbnailimage123456",
         .height = 1,
         .width = 1,
     };
-
+    // Fake video for now. Will replace with default video to test some functionalities later on.
     const default_video = YtTypes.YtVideo{
         .video_id = 1,
         .channel_id = 1,
